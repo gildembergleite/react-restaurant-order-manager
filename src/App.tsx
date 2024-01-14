a31c05c1f332f53a890aeb6bf0e87fdd3727a181
Gildemberg Leite <gildembergleite@gmail.com>
2024-01-14 19:35:04 -0300

    feat: add React Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
-  return <h1>Sign In</h1>
+  return (
+    <>
+      <Helmet title='Login' />
+      <h1>Sign In</h1>
+    </>
+  )
 }
