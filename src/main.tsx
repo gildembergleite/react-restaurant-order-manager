4bbbfd68aaf500e8a8a4cab5143b1d2593c54e40
Gildemberg Leite <gildembergleite@gmail.com>
2024-01-21 10:18:05 -0300

    feat: add Toaster provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import './globals.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'sonner'
+
 import { App } from './App.tsx'
-import './globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toaster position='top-right' richColors />
     <App />
   </React.StrictMode>,
 )
