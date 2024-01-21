f27974cea66009c9e8145cbf6f3fd9985e168576
Gildemberg Leite <gildembergleite@gmail.com>
2024-01-21 10:20:02 -0300

    feat: add sign in form in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,24 @@
 import { Helmet } from 'react-helmet-async'
 
+import { SignInForm } from './sign-in-form'
+
 export function SignIn() {
   return (
     <>
       <Helmet title='Login' />
-      <h1>Sign In</h1>
+      <div className='p-8'>
+        <div className='w-[350px] flex flex-col justify-center gap-6'>
+          <div className='space-y-2 text-center'>
+            <h1 className='text-2xl font-bold'>
+              Acessar painel
+            </h1>
+            <p className='text-sm text-muted-foreground'>
+              Acompanhe suas vendas pelo painel do parceiro.
+            </p>
+          </div>
+          <SignInForm />
+        </div>
+      </div>
     </>
   )
 }
