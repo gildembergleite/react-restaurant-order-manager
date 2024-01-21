70e58f0623a76b47620834a33b9700826f94aa0d
Gildemberg Leite <gildembergleite@gmail.com>
2024-01-21 10:18:29 -0300

    feat: create auth layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,21 @@
+import { PizzaIcon } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <div>
+    <main className='min-h-screen grid grid-cols-2'>
+      <div className='h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between'>
+        <div className='flex items-center gap-3 text-lg font-medium text-foreground'>
+          <PizzaIcon size={20} />
+          <span className='font-semibold'>pizza.shop</span>
+        </div>
+        <footer>
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className='flex justify-center items-center'>
         <Outlet />
       </div>
-    </div>
+    </main>
   )
 }
