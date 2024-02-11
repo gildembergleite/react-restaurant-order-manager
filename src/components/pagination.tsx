d458d36e2567fdf8f5ab73682cb99ba26a0ac029
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-11 09:08:55 -0300

    feat: disabled Buttons

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -13,6 +13,9 @@ export default function Pagination(props: PaginationProps) {
   const { pageIndex, totalCount, perPage, onPageChange } = props
   const pages = Math.ceil(totalCount / perPage) || 1
 
+  const isTheFirstPage = pageIndex === 0
+  const isTheLastPage = pageIndex === pages - 1
+
   return (
     <div className="flex items-center justify-between">
       <span className="text-sm text-muted-foreground">
@@ -25,22 +28,22 @@ export default function Pagination(props: PaginationProps) {
         </div>
 
         <div className="flex items-center gap-2">
-          <Button onClick={() => onPageChange(0)} variant={'outline'} className="w-8 h-8 p-0">
+          <Button disabled={isTheFirstPage} onClick={() => onPageChange(0)} variant={'outline'} className="w-8 h-8 p-0">
             <ChevronsLeftIcon size={16} />
             <span className="sr-only">Primeira página</span>
           </Button>
 
-          <Button onClick={() => onPageChange(pageIndex - 1)} variant={'outline'} className="w-8 h-8 p-0">
+          <Button disabled={isTheFirstPage} onClick={() => onPageChange(pageIndex - 1)} variant={'outline'} className="w-8 h-8 p-0">
             <ChevronLeftIcon size={16} />
             <span className="sr-only">Próxima página</span>
           </Button>
 
-          <Button onClick={() => onPageChange(pageIndex + 1)} variant={'outline'} className="w-8 h-8 p-0">
+          <Button disabled={isTheLastPage} onClick={() => onPageChange(pageIndex + 1)} variant={'outline'} className="w-8 h-8 p-0">
             <ChevronRightIcon size={16} />
             <span className="sr-only">Página anterior</span>
           </Button>
 
-          <Button onClick={() => onPageChange(pages - 1)} variant={'outline'} className="w-8 h-8 p-0">
+          <Button disabled={isTheLastPage} onClick={() => onPageChange(pages - 1)} variant={'outline'} className="w-8 h-8 p-0">
             <ChevronsRightIcon size={16} />
             <span className="sr-only">Última página</span>
           </Button>
