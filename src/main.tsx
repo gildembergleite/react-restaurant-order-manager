df9b9d4687df269d8bb75ceb0df6d88dddb1735e
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-12 21:07:52 -0300

    feat: setup mocks with msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import './globals.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { enableMSW } from './api/mocks/index.ts'
 import { App } from './app.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+enableMSW().then(() => (
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+))
